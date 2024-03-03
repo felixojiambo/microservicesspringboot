401b060932b1cbe434c47ac5d0654a1b9d600026
Felix Ojiambo <felixojiamboe@gmail.com>
2024-03-03 04:33:11 -0800

    controllers  definition

---
+++ src/main/java/felix/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package felix.productservice.repository;
+
+import felix.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product,String> {
+
+}
